9b377700e30f15088b14ea763d4308654e3430f7
lolambl <130396801+lolambl@users.noreply.github.com>
2023-10-17 21:54:05 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+var image = '<img class="card-img" src="image/mysslie.JPG" alt="image-mysslie">/'
+
+$(".btn-danger").on("click", function() {
+    $("#result").html("");
+    $("#result").append(image)
+}
+);
